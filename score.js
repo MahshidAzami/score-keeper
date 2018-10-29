61226378da4e2edf34de188dfa4b4d36dc88c108
monaazami <32145698+monaazami@users.noreply.github.com>
2018-10-29 21:36:41 +0000

    added todo list and changed the list when hovering or clicking them

--- score.js
+++ score.js
@@ -52,3 +52,26 @@ numInput.addEventListener("change",function(){
     winningScore = Number(numInput.value);
     reset();
 })
+
+
+
+// var firstLi = document.querySelector("li");
+// firstLi.addEventListener("mouseover",function(){
+//    firstLi.style.color="green";
+// });
+// firstLi.addEventListener("mouseout", function(){
+//     firstLi.style.color= "black";
+// });
+
+var lis = document.querySelectorAll("li");
+for(var i= 0; i<lis.length ; i++){
+     lis[i].addEventListener("mouseover",function(){
+        this.style.color="green";
+     });
+     lis[i].addEventListener("mouseout", function(){
+        this.style.color= "black";
+     });
+     lis[i].addEventListener("click",function(){
+         this.classList.toggle("done");
+     })
+}
