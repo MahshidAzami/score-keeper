0458fc82f84dc6d0d33363143a9a3b3f2b9b65cd
monaazami <32145698+monaazami@users.noreply.github.com>
2018-10-29 16:23:15 +0000

    reset butten

--- score.js
+++ score.js
@@ -3,6 +3,8 @@ var p2Button = document.querySelector("#p2");
 var resetButton = document.querySelector("#reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -31,13 +33,22 @@ p2Button.addEventListener("click", function(){
         p2Display.textContent=p2Score;
     } 
 });
-resetButton.addEventListener("click",function(){
-    var p1Score = 0;
-    var p2Score = 0;
-    var gameOver = false;
+resetButton.addEventListener("click",reset);
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
     p1Display.classList.remove("winner");
     p2Display.classList.remove("winner");
     p2Display.textContent=p2Score;
     p1Display.textContent=p1Score;
-    var gameOver = false;
-});
+    gameOver = false;
+}
+
+
+console.log(numInput);
+
+numInput.addEventListener("change",function(){
+    winningScoreDisplay.textContent=numInput.value;
+    winningScore = Number(numInput.value);
+    reset();
+})
